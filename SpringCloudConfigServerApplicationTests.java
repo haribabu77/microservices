2adbab123280e883789c9ef2d368bb63ba0e7d25
HariBabu <hguraka@gmail.com>
2020-07-29 02:56:29 -0500

    Added profiles in spring-cloud-config

---
+++ SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hari.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
